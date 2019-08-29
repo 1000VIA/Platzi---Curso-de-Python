6a1eefd4f644c4f3d1a798a614f0dc270a157e7e
1000VIA <milvampa1@gmail.com>
2019-08-29 11:12:02 -0500

    comments

--- ejemplo_inicial.py
+++ ejemplo_inicial.py
@@ -1,9 +1,10 @@
+#intalar sudo pacman -S tk   
 import turtle
 
-window = turtle.Screen()
-milvia = turtle.Turtle()
-milvia.forward(50)
-milvia.left(90)
+window = turtle.Screen() # Decirle a turtle que queremos generar una ventana
+milvia = turtle.Turtle() # Genero una tortuga que se va a llamar 'Milvia'
+milvia.forward(50) #Que se mueva una distancia de 50 de manera derecha.
+milvia.left(90) #Que de una vuelta de 90º
 milvia.forward(50)
 milvia.left(90)
 milvia.forward(50)
@@ -11,4 +12,4 @@ milvia.left(90)
 milvia.forward(50)
 milvia.left(90)
 
-turtle.mainloop();
+turtle.mainloop(); #mainloop -> es para que turtle no cierre la ventana
