cb4d12e1bf42e929b5ae9ef825b41c370f06178d
1000VIA <milvampa1@gmail.com>
2019-08-29 07:15:51 -0500

    Ejemplo inicial

---
+++ ejemplo_inicial.py
@@ -0,0 +1,14 @@
+import turtle
+
+window = turtle.Screen()
+milvia = turtle.Turtle()
+milvia.forward(50)
+milvia.left(90)
+milvia.forward(50)
+milvia.left(90)
+milvia.forward(50)
+milvia.left(90)
+milvia.forward(50)
+milvia.left(90)
+
+turtle.mainloop();
