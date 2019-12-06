8bb06a13e61f98db674b7b584e16f9b809a385c4
1000VIA <milvampa1@gmail.com>
2019-12-05 19:17:06 -0500

    exercise protected function

---
+++ lamp.py
@@ -0,0 +1,60 @@
+class Lamp:
+    _LAMPS = ['''
+          .
+     .    |    ,
+      \   '   /
+       ` ,-. '
+    --- (   ) ---
+         \ /
+        _|=|_
+       |_____|
+    ''',
+              '''
+         ,-.
+        (   )
+         \ /
+        _|=|_
+       |_____|
+    ''']
+
+    # Primer método que se va a llamar, self indica que un método de instancia
+    def __init__(self, is_turned_on):
+        self._is_turned_on = is_turned_on  # nace estando apagada
+
+    def turn_on(self):  # Método público para prender la lampara
+        self._is_turned_on = True
+        self._display_image()
+
+    def turn_off(self):  # Método público para apagar la lampara
+        self._is_turned_on = False
+        self._display_image()
+
+    def _display_image(self):  # Método privado
+        if self._is_turned_on:
+            print(self._LAMPS[0])
+        else:
+            print(self._LAMPS[1])
+
+
+def run():
+    lamp = Lamp(is_turned_on=False)
+
+    while True:
+        command = str(input('''
+        ¿Que deseas hacer?
+
+        [p]render
+        [a]pagar
+        [s]alir
+        '''))
+
+        if command == 'p':
+            lamp.turn_on()
+        elif command == 'a':
+            lamp.turn_off()
+        else:
+            break
+
+
+if __name__ == '__main__':
+    run()
