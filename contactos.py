65bead8e89230e5937f72f729bcbe7cb55a88003
1000VIA <milvampa1@gmail.com>
2019-12-12 18:54:59 -0500

    method update

--- contactos.py
+++ contactos.py
@@ -32,6 +32,29 @@ class ContactBook:
                 break
         else:
             self._not_found()
+    
+    def update(self, name, phone, email):
+        for contact in self._contacts:
+            if contact.name.lower() == name.lower():
+                contact.phone = phone
+                contact.email = email
+                break
+        else:
+            self._not_found()
+
+    
+    def find(self, name):
+        for idx, contact in enumerate(self._contacts):
+            if contact.name.lower() == name.lower():
+                return idx
+        else:
+            return-1
+
+    def update(self, idx, name, phone, email):
+        newContact = Contact(name, phone, email)
+        self._contacts[idx] = newContact
+        print('Contacto actualizado')
+        print('*---*---*---*---*---*---*---*---*')
 
     def _print_contact(self, contact): #los metodos de instancia siempre empiesan con self
         print('*---*---*---*---*---*---*---*---*')
@@ -70,15 +93,23 @@ def run():
             contact_book.add(name, phone, email)
 
         elif command == 'ac':
-            print('actualizar contacto')
+            name = str(input('Escribe el nombre del contacto a actualizar: '))
+            idx = contact_book.find(name)
+            if idx>=0:
+                name = str(input('Escribe el nuevo nombre del contacto: '))
+                phone = str(input('Escribe el telefono del contacto: '))
+                email = str(input('Escribe el email del contacto: '))
+                contact_book.update(idx, name, phone, email)
+            else:
+                print('No encontrado')
 
         elif command == 'b':
-            name = str(input('Escribe el nombre del contacto: '))
+            name = str(input('Escribe el nombre del contacto a buscar: '))
 
             contact_book.search(name)
 
         elif command == 'e':
-            name = str(input('Escribe el nombre del contacto: '))
+            name = str(input('Escribe el nombre del contacto para eliminar: '))
 
             contact_book.delete(name)
 
@@ -94,5 +125,6 @@ def run():
 
 
 if __name__ == '__main__':
+    print('')
     print('B I E N V E N I D O  A  L A  A G E N D A')
     run()
